8df634adac57bfdcbe388ce2cb2446039008f6ed
Ian <ianswensson@gmail.com>
2021-12-02 19:12:54 -0700

    project set up

---
+++ backend/db/conn.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * https://zellwk.com/blog/local-mongodb/
+ * I used the above link to create a mongoose connection
+ * 
+ * https://www.mongodb.com/languages/express-mongodb-rest-api-tutorial
+ * I used the above link to map routes
+ * 
+ * https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+ * 
+ */
+
+const mongoose = require('mongoose');
+const dbName = 'minesweeper-rpg';
+const url = `mongodb://localhost:27017/${dbName}`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.once('open', _ => {
+  console.log('Connected to mongoose: ', url);
+  console.log('Using database: ', dbName);
+});
+db.on('error', err => {
+  console.error('connection error in server/db/conn.js: ', err);
+});
+
+module.exports = db;
